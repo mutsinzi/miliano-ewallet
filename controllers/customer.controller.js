9cc0c48d976d99f85bb8d372b7e474473c44e12a
mutsinzi <mutsinzi@proton.me>
2024-03-29 14:46:25 +0200

    include jwt in customer creation response

--- controllers/customer.controller.js
+++ controllers/customer.controller.js
@@ -1,13 +1,23 @@
 import Customer from '../models/customer.model.js';
+import { generateToken } from '../utils/token.util.js';
 
 // Register a new customer
 export const registerCustomer = async (req, res) => {
   const { name, email, password } = req.body;
   try {
+    const customerExists = await Customer.findOne({ email });
+    if (customerExists) {
+      return res.status(400).json({ message: "Customer already exists" });
+    }
+
     const customer = await Customer.create({ name, email, password });
-    customer.password = undefined;
-    customer.__v = undefined;
-    res.respond(customer, 'Customer registered successfully', 201)
+
+    res.respond({
+      _id: customer._id,
+      name: customer.name,
+      email: customer.email,
+      token: generateToken(customer._id),
+    }, 'Customer registered successfully', 201)
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
