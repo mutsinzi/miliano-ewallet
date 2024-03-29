75bd3e7af4ec388b216966c842960507502b69a8
mutsinzi <156684911+emutsinz@users.noreply.github.com>
2024-03-29 13:45:55 +0200

    add transaction controller: create and read all

---
+++ controllers/transaction.controller.js
@@ -0,0 +1,78 @@
+import mongoose from 'mongoose';
+import Transaction from '../models/transaction.model.js';
+import Wallet from '../models/wallet.model.js';
+
+export const createTransaction = async (req, res) => {
+  const session = await mongoose.startSession();
+  session.startTransaction();
+
+  try {
+    const { walletId, type, amount, description } = req.body;
+    const opts = { session, new: true }; 
+
+    const wallet = await Wallet.findById(walletId).session(session);
+    if (!wallet) {
+      await session.abortTransaction();
+      session.endSession();
+      return res.status(404).json({ message: "Wallet not found" });
+    }
+
+    // Update wallet balance based on transaction type
+    if (type === 'credit') {
+      wallet.balance += amount;
+    } else if (type === 'debit') {
+      if (wallet.balance < amount) {
+        await session.abortTransaction();
+        session.endSession();
+        return res.status(400).json({ message: "Insufficient balance" });
+      }
+      wallet.balance -= amount;
+    }
+
+    await wallet.save(opts);
+
+    // Create and save the transaction
+    const transaction = new Transaction({ walletId, type, amount, description });
+    await transaction.save(opts);
+
+    await session.commitTransaction(); 
+    session.endSession(); 
+
+    res.respond({ transactionId: transaction._id, currentBalance: wallet.balance }, 'Transaction created successfully', 201);
+  } catch (error) {
+    await session.abortTransaction(); 
+    session.endSession();
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const getTransactions = async (req, res) => {
+  try {
+    const transactions = await Transaction.aggregate([
+      {
+        $lookup: {
+          from: 'wallets',
+          localField: 'walletId',
+          foreignField: '_id',
+          as: 'walletDetails'
+        }
+      },
+      {
+        $unwind: '$walletDetails' 
+      },
+      {
+        $project: {
+          type: 1,
+          amount: 1,
+          description: 1,
+          createdAt: 1,
+          updatedAt: 1,
+          'currency': '$walletDetails.currency' 
+        }
+      }
+    ]);
+    res.respond(transactions, 'Fetched transactions successfully');
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
