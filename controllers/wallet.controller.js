eb15d574e3b86e268ad4f8c49a70adebc300b695
mutsinzi <mutsinzi@proton.me>
2024-03-29 14:45:30 +0200

    add redis caching

--- controllers/wallet.controller.js
+++ controllers/wallet.controller.js
@@ -1,3 +1,4 @@
+import redis from '../config/redis.js';
 import Wallet from '../models/wallet.model.js';
 
 export const createWallet = async (req, res) => {
@@ -5,6 +6,10 @@ export const createWallet = async (req, res) => {
   try {
     const wallet = new Wallet({ customerId, balance, currency });
     await wallet.save();
+
+    // Invalidate redis cache
+    await redis.del('wallets');
+
     res.respond(wallet, 'Wallet created successfully', 201)
   } catch (error) {
     res.status(400).json({ message: error.message });
@@ -13,8 +18,17 @@ export const createWallet = async (req, res) => {
 
 export const getWallets = async (req, res) => {
   try {
+    const cachedWallets = await redis.get('wallets');
+    if (cachedWallets) {
+      return res.respond(JSON.parse(cachedWallets), 'Wallets fetched successfully from cache');
+    }
+
     const wallets = await Wallet.find();
-    res.respond(wallets, 'Wallets fetched successfully')
+
+    // Store the wallets list in Redis
+    await redis.set('wallets', JSON.stringify(wallets), 'EX', 3600);
+
+    res.respond(wallets, 'Wallets fetched successfully');
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
